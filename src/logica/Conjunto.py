beac4279926716114c0a6261dabcdfda8272a996
Yorshgamer <75707785@continental.edu.pe>
2024-06-05 16:06:39 -0500

    Se añade promedio ponderado

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -1,10 +1,17 @@
 class Conjunto:
     def __init__(self, conjunto):
-        self.__conjunto=conjunto
+        self.__conjunto = conjunto
 
-    def promedio( self ):
+    def promedio(self):
         if len(self.__conjunto) > 0:
             return sum(self.__conjunto) / len(self.__conjunto)
         else:
             return None
 
+    def promedio_ponderado(self, pesos):
+        if len(self.__conjunto) == len(pesos):
+            suma_ponderada = sum(valor * peso for valor, peso in zip(self.__conjunto, pesos))
+            suma_pesos = sum(pesos)
+            return suma_ponderada / suma_pesos if suma_pesos != 0 else None
+        else:
+            return None
--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -1,7 +1,6 @@
 import unittest
-from src.logica.Conjunto import Conjunto
-
-class TestConjunto( unittest.TestCase ):
+from KataTDD.src.logica.Conjunto import Conjunto
+class TestConjunto(unittest.TestCase):
     def test_conjunto_vacio_retornaNone(self):
         conjunto = Conjunto([])
         self.assertIsNone(conjunto.promedio())
@@ -9,14 +8,19 @@ class TestConjunto( unittest.TestCase ):
     def test_conjunto_unElemento_retornaValorUnicoElemento(self):
         conjunto = Conjunto([5])
         self.assertEqual(5, conjunto.promedio())
-    def test_conjunto_dosElementos_retornaPromedioElementos( self ):
-        conjunto=Conjunto([5,7])
-        self.assertEqual(6,conjunto.promedio())
+
+    def test_conjunto_dosElementos_retornaPromedioElementos(self):
+        conjunto = Conjunto([5, 7])
+        self.assertEqual(6, conjunto.promedio())
 
     def test_conjunto_nElementos_retornaPromedioNElementos(self):
         conjunto = Conjunto([2, 4, 8, 9, 10, 15])
         self.assertEqual((2 + 4 + 8 + 9 + 10 + 15) / 6, conjunto.promedio())
 
+    def test_promedio_ponderado(self):
+        conjunto = Conjunto([2, 3, 4])
+        pesos = [1, 2, 3]
+        self.assertAlmostEqual(3.3333333333333335, conjunto.promedio_ponderado(pesos), places=10)
 
 if __name__ == '__main__':
     unittest.main()
